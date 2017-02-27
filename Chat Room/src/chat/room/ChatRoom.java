16244490a4e4507cf9901fd789b601ea14712c85
Strepsdew <ew.potato@gmail.com>
2017-02-27 13:33:18 +0200

    Revert "Register form"

--- Chat Room/src/chat/room/ChatRoom.java
+++ Chat Room/src/chat/room/ChatRoom.java
@@ -1,8 +1,6 @@
 package chat.room;
 public class ChatRoom {
     public static void main(String[] args) {
-        
-        
 //        login k = new login();
     }
     
--- Chat Room/src/chat/room/login.java
+++ Chat Room/src/chat/room/login.java
@@ -1,11 +1,16 @@
 package chat.room;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.Scanner;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
