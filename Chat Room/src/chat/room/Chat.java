d6d8b3a6c5d69a6187210bb853b3ea57c0e7ed52
Strepsdew <ew.potato@gmail.com>
2017-03-29 14:17:31 +0300

    send messagen alotuksia

--- Chat Room/src/chat/room/Chat.java
+++ Chat Room/src/chat/room/Chat.java
@@ -2,14 +2,21 @@ package chat.room;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Image;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
@@ -24,18 +31,18 @@ public class Chat extends JFrame {
     private JPanel alaosa = new JPanel(new BorderLayout());
 
     private JTextArea viesti = new JTextArea();
-    private JButton closebtn = new JButton();
+    private JButton backBtn = new JButton();
     private JButton Frendbtn = new JButton();
-    private JButton Sendbtn = new JButton();
-    private JTextArea chatarea = new JTextArea();
+    private JButton sendBtn = new JButton();
+    private JTextArea chatArea = new JTextArea();
 
     private int currentUserId;
     Client client = new Client("localhost", 1500, null, this);
     
     private boolean connected;
 
-    public Chat() {
-       
+    public Chat() throws IOException {
+       sendImage();
         this.setTitle("Chat with KAVERIN_NIMI"); // tähän lisätään chat with kaverin nimi
         this.setSize(410, 350);
         this.setLocationRelativeTo(null);
@@ -44,46 +51,55 @@ public class Chat extends JFrame {
         viesti.setBorder(BorderFactory.createCompoundBorder(b,
                 BorderFactory.createEmptyBorder(10, 10, 10, 10)));
 //        viesti.setText("kirjoitat tähän");
-        Sendbtn.setText("Send");
-        closebtn.setText("close");
+        sendBtn.setText("Send");
+        backBtn.setText("Back");
         asetteleKomponentit();
         this.setVisible(true);
-        chatarea.setEditable(false);
-       Sendbtn.setBackground(Color.GRAY);
-        Sendbtn.setForeground(Color.BLACK);
-        viesti.requestFocus();
+        chatArea.setEditable(false);
+       sendBtn.setBackground(Color.GRAY);
+        sendBtn.setForeground(Color.BLACK);
+        viesti.setText("kirjoitat tähän");
+        chatArea.requestFocus();
 
         Border roundedBorder = new LineBorder(null, 2, true); // the third parameter - true, says it's round
          viesti.setBorder(roundedBorder); 
          //yritän tehdä kirjoitus areasta pyöreän
-        viesti.addKeyListener(new KeyListener() {
-            int jotain;
+         
+         
+         chatArea.addKeyListener(new KeyListener(){
+             @Override
+            public void keyTyped(KeyEvent e) {}
             
-
             @Override
-            public void keyTyped(KeyEvent e) {
-                
-            }
+            public void keyPressed(KeyEvent e) {viesti.requestFocus();}
+                  
+            @Override
+            public void keyReleased(KeyEvent e) {}
+         });
+         
+         
+        viesti.addKeyListener(new KeyListener() {
+            
+            @Override
+            public void keyTyped(KeyEvent e) {}
 
             @Override
             public void keyReleased(KeyEvent e) {
                 if (e.getKeyCode() == KeyEvent.VK_ENTER) {
 
                     if (connected) {
-                        if(!viesti.getText().equals("\n")){
+                        
                         client.sendMessage(new ChatMessage(ChatMessage.MESSAGE, viesti.getText()));
                         viesti.setText("");
-                        viesti.requestFocus();
+                        
                         return;
-                        }
+                        
                     }
                 }
             }
 
             @Override
-            public void keyPressed(KeyEvent e) {
-            }
-            
+            public void keyPressed(KeyEvent e) {}
         });
         
 viesti.addFocusListener(new FocusListener() {
@@ -116,25 +132,25 @@ viesti.addFocusListener(new FocusListener() {
 
     private void asetteleKomponentit() {
         ylaosa.add(Frendbtn, BorderLayout.LINE_START);
-        ylaosa.add(closebtn, BorderLayout.LINE_END);
+        ylaosa.add(backBtn, BorderLayout.LINE_END);
         pohja.add(ylaosa, BorderLayout.PAGE_START);
-        keskiosa.add(new JScrollPane(chatarea));
+        keskiosa.add(new JScrollPane(chatArea));
         pohja.add(keskiosa, BorderLayout.CENTER);
         viesti.setBounds(100, 300, 200, 200);
         alaosa.add(viesti, BorderLayout.CENTER);
-        alaosa.add(Sendbtn, BorderLayout.LINE_END);
+        alaosa.add(sendBtn, BorderLayout.LINE_END);
         pohja.add(alaosa, BorderLayout.PAGE_END);
         this.add(pohja);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         new Chat();
 
     }
 
     void append(String str) {
-        chatarea.append(str);
-        chatarea.setCaretPosition(chatarea.getText().length() - 1);
+        chatArea.append(str);
+        chatArea.setCaretPosition(chatArea.getText().length() - 1);
     }
 
     public void run() {
@@ -144,5 +160,33 @@ viesti.addFocusListener(new FocusListener() {
         connected = true;
 
     }
+    
+    public void sendImage() throws IOException{
+        
+        JFileChooser chooser = new JFileChooser();
+            int status = chooser.showOpenDialog(null);
+            if (status == JFileChooser.APPROVE_OPTION) {
+                File file = chooser.getSelectedFile();
+                if (file == null) {
+                    return;
+                }
+
+               String  fileName = chooser.getSelectedFile().getAbsolutePath();
+               
+                   File sourceimage = new File(fileName);
+                Image image = ImageIO.read(sourceimage);
+                   
+              
+                JFrame frame = new JFrame();
+                 frame.setSize(300, 300);
+                JLabel label = new JLabel(new ImageIcon(image));
+                frame.add(label);
+                 frame.setVisible(true);
+
+
+
+            }
+    }
+    
 
 }
