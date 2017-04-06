3906043f0077ea2ba977251209589936b3a01201
ferealqq <ferealq@gmail.com>
2017-04-06 10:11:10 +0300

    Pientä korjausta ja bio kohdan lisääminen

--- src/chat/room/InffoIkkuna.java
+++ src/chat/room/InffoIkkuna.java
@@ -11,19 +11,20 @@ import javax.swing.JTextField;
 
 public class InffoIkkuna extends JFrame{
     
-    private JPanel pohja = new JPanel(new GridLayout(5,1));
+    private JPanel pohja = new JPanel(new GridLayout(6,1));
     private JTextField etunimi = new JTextField("");
     private JTextField sukunimi = new JTextField("");
     private JTextField nickname = new JTextField("");
     private JTextField username = new JTextField("");
     private JTextField email = new JTextField("");
+    private JTextField bio  = new JTextField("");
 
     private int id;
     public InffoIkkuna(int id) {
         this.setUndecorated(true);
         this.id = id;
         Database k = new Database();
-        this.setSize(150,100);
+        this.setSize(200,100);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         asetteleKomponentit();
@@ -36,6 +37,7 @@ public class InffoIkkuna extends JFrame{
         nickname.setEditable(false);
         username.setEditable(false);
         email.setEditable(false);
+        bio.setEditable(false);
         Profiili profiili = k.getEverythingById(id);
         etunimi.setText("Etunimi : " + profiili.getEtunimi());
         sukunimi.setText("Sukunimi : "+profiili.getSukunimi());
@@ -43,6 +45,13 @@ public class InffoIkkuna extends JFrame{
         username.setText("Username : "+profiili.getUsername());
         if(profiili.getEmail() != null) {
             email.setText("Email : "+profiili.getEmail());
+        }else{
+            email.setText("Email :");
+        }
+        if(profiili.getBio() != null) {
+            bio.setText("Bio : "+profiili.getBio());
+        }else{
+            bio.setText("Bio :");
         }
     }
     private void asetteleKomponentit() {
@@ -51,6 +60,7 @@ public class InffoIkkuna extends JFrame{
         pohja.add(nickname);
         pohja.add(username);
         pohja.add(email);
+        pohja.add(bio); 
         this.add(pohja);
     }
     public BufferedImage resize(BufferedImage img, int newW, int newH) {
